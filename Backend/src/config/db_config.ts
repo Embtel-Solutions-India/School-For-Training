73611b5d424304747dd816214e5512e770c51d36
Ujjwal Anand <ujjwal.anand6376@gmail.com>
2026-04-17 05:33:22 +0530

    Refactor db_config to simplify database configuration and remove unused variables

--- Backend/src/config/db_config.ts
+++ Backend/src/config/db_config.ts
@@ -1,16 +1,9 @@
-
 import { config } from 'dotenv';
-import { ENVIRONMENT, LOCAL_PORT, PROD_PORT, LOCAL_DB_URL } from '../../env_config';
 config();
 
-const env = ENVIRONMENT
-let server_port = LOCAL_PORT
-const local_db_url = LOCAL_DB_URL
-if (env == 'PROD') { server_port = PROD_PORT }
 const db_config = {
-  PORT: server_port,
-  URI: `${local_db_url}`
-}
+  PORT: process.env.PORT || 4000,
+  URI: process.env.LOCAL_DB_URL || ""
+};
 
-
-export default db_config
+export default db_config;
